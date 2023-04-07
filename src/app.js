d58e3f77cf95a8640466f7f9176e3b5ee0f3d193
Robert <robalmendares@gmail.com>
2023-04-06 21:14:09 -0700

    3001 -> 3000

--- src/app.js
+++ src/app.js
@@ -98,6 +98,6 @@ app.get ('*', (req, res)=> {
 
 
 
-app.listen(3001, () => {
-    console.log('Server is up on port 3001.')
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
