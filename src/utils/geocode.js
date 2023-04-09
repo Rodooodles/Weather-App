102b0357181680e8ca8513c3060548ac6b45a417
Robert <robalmendares@gmail.com>
2023-04-09 16:29:27 -0700

    trying the fix the fetch

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoicm9kb29kbGVzIiwiYSI6ImNsZmxoM2p5ejAxdDU0N3BpNDBoamgwNXUifQ.vD-8nNjL3QhgitXLucp6Kw&limit=1&autocomplete=false&proximity=ip'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoicm9kb29kbGVzIiwiYSI6ImNsZmxoM2p5ejAxdDU0N3BpNDBoamgwNXUifQ.vD-8nNjL3QhgitXLucp6Kw&limit=1&autocomplete=false'
 
     request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
